442f2ddb4bb16ef763f23baff4d5fdc85d82ba15
udbhavbalaji <udbhavbalaji@gmail.com>
2025-10-11 12:38:41 -0400

    created basic cli app

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 #!usr/bin/env bun
 
 import createCLI from "@/index";
+import { version, description } from "./package.json";
+
+const app = createCLI(version, description);
+
+app.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-function createCLI() { }
+import { Command } from "commander";
+
+function createCLI(appVersion: string, appDescription: string): Command {
+  const app = new Command("cvstack")
+    .description(appDescription)
+    .version(appVersion)
+    .showSuggestionAfterError(true)
+    // .addHelpText("beforeAll", getBanner())
+    .allowExcessArguments(true)
+    .alias("cvs");
+
+  // add other commands here
+  app.action(() => console.log("App working"));
+
+  return app;
+}
 
 export default createCLI;
